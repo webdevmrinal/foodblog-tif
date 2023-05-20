1d2bd484d7e8d68d5071f285c3c20940723dff2d
webdevmrinal <mrinal.webdev@gmail.com>
2023-05-20 20:48:31 +0530

    first commit

---
+++ src/pages/LandingPageDesktop.js
@@ -0,0 +1,245 @@
+import "./LandingPageDesktop.css";
+
+const LandingPageDesktop = () => {
+  return (
+    <div className="landing-page-desktop">
+      <div className="landing-page-desktop-child" />
+      <img
+        className="screenshot-669-1-icon"
+        alt=""
+        src="/screenshot-669-1@2x.png"
+      />
+      <img
+        className="landing-page-desktop-item"
+        alt=""
+        src="/rectangle-404.svg"
+      />
+      <div className="landing-page-desktop-inner" />
+      <img className="rectangle-icon" alt="" src="/rectangle-400@2x.png" />
+      <footer className="frame-parent">
+        <div className="frame-group">
+          <section className="contact-us-parent">
+            <div className="contact-us">Contact Us</div>
+            <div className="lorem-ipsum-pvt-ltd51-magal-parent">
+              <div className="lorem-ipsum-pvt">
+                Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM
+                Market, XYZ-343434
+              </div>
+              <div className="example2020gmailcom">example2020@gmail.com</div>
+              <div className="div">(904) 443-0343</div>
+            </div>
+          </section>
+          <section className="more-parent">
+            <div className="more">More</div>
+            <div className="social-links">Social Links</div>
+            <div className="products-parent">
+              <div className="products">Products</div>
+              <div className="career">Career</div>
+              <div className="contact-us1">Contact Us</div>
+              <div className="about-us">About Us</div>
+            </div>
+          </section>
+          <div className="food-truck-example">© 2022 Food Truck Example</div>
+        </div>
+        <img className="component-icon" alt="" src="/component.svg" />
+        <img className="component-icon1" alt="" src="/component1.svg" />
+        <img className="path-3-icon" alt="" src="/path-3.svg" />
+      </footer>
+      <button className="explore-now">
+        <b className="explore-now1">Explore Now!</b>
+      </button>
+      <div className="discover-the-best-container">
+        <p className="discover-the">{`Discover the `}</p>
+        <p className="discover-the">
+          <span className="best">Best</span>
+          <span> Food and Drinks</span>
+        </p>
+      </div>
+      <div className="naturally-made-healthcare">{`Naturally made Healthcare Products for the better care & support of your body.`}</div>
+      <img className="aboutgirl-lp-icon" alt="" src="/aboutgirl-lp.svg" />
+      <div className="rectangle-div" />
+      <img
+        className="screenshot-669-2-icon"
+        alt=""
+        src="/screenshot-669-2@2x.png"
+      />
+      <img className="vector-icon" alt="" src="/vector-1.svg" />
+      <button className="get-in-touch-wrapper">
+        <div className="get-in-touch">Get In Touch</div>
+      </button>
+      <img className="landing-page-desktop-child1" alt="" src="/vector-2.svg" />
+      <article className="latest-articles-parent">
+        <div className="latest-articles">Latest Articles</div>
+        <section className="grilled-tomatoes-1-846x846-1-parent">
+          <img
+            className="grilled-tomatoes-1-846x846-1-icon"
+            alt=""
+            src="/grilledtomatoes1846x846-1@2x.png"
+          />
+          <div className="frame-container">
+            <article className="grilled-tomatoes-at-home-parent">
+              <b className="grilled-tomatoes-at">Grilled Tomatoes at Home</b>
+              <div className="plorem-ipsum-is">
+                PLorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard...
+              </div>
+            </article>
+            <button className="read-more-wrapper">
+              <div className="read-more">Read More</div>
+            </button>
+          </div>
+        </section>
+        <section className="frame-section">
+          <div className="frame-wrapper">
+            <div className="frame-div">
+              <div className="snacks-for-travel-parent">
+                <b className="grilled-tomatoes-at">Snacks for Travel</b>
+                <div className="plorem-ipsum-is">
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <button className="read-more-wrapper">
+                <div className="read-more">Read More</div>
+              </button>
+            </div>
+          </div>
+          <img
+            className="meal-prep-ideas-846x846-1-icon"
+            alt=""
+            src="/mealprepideas846x846-1@2x.png"
+          />
+        </section>
+        <section className="frame-parent1">
+          <div className="frame-wrapper">
+            <div className="frame-div">
+              <div className="snacks-for-travel-parent">
+                <b className="grilled-tomatoes-at">Post-workout Recipes</b>
+                <div className="plorem-ipsum-is">
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <button className="read-more-frame">
+                <div className="read-more">Read More</div>
+              </button>
+            </div>
+          </div>
+          <img
+            className="meal-prep-ideas-846x846-1-icon"
+            alt=""
+            src="/mealprepideas846x846-11@2x.png"
+          />
+        </section>
+      </article>
+      <section className="frame-parent3">
+        <div className="frame-wrapper2">
+          <div className="about-us-parent">
+            <div className="about-us1">About Us</div>
+            <div className="lorem-ipsum-is">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. t has survived
+              not only five centuries.
+            </div>
+          </div>
+        </div>
+        <button className="group-button">
+          <div className="read-more3">Read More</div>
+        </button>
+      </section>
+      <div className="div1">1/2</div>
+      <button className="image-1-traced-wrapper">
+        <img className="image-1-traced" alt="" src="/image-1-traced.svg" />
+      </button>
+      <button className="image-1-traced-parent">
+        <img className="image-1-traced1" alt="" src="/image-1-traced1.svg" />
+        <img className="image-1-traced1" alt="" src="/image-1-traced1.svg" />
+        <div className="group-child" />
+      </button>
+      <div className="landing-page-desktop-child2" />
+      <div className="div2">2/2</div>
+      <button className="image-1-traced-container">
+        <img className="image-1-traced" alt="" src="/image-1-traced2.svg" />
+      </button>
+      <button className="image-1-traced-group" disabled>
+        <img className="image-1-traced1" alt="" src="/image-1-traced3.svg" />
+        <img className="image-1-traced1" alt="" src="/image-1-traced4.svg" />
+        <div className="group-item" />
+      </button>
+      <article className="latest-articles-group">
+        <div className="latest-articles">Latest Articles</div>
+        <article className="grilled-tomatoes-1-846x846-1-group">
+          <img
+            className="grilled-tomatoes-1-846x846-1-icon"
+            alt=""
+            src="/grilledtomatoes1846x846-11@2x.png"
+          />
+          <div className="frame-container">
+            <div className="how-to-grill-corn-parent">
+              <b className="grilled-tomatoes-at">How To Grill Corn</b>
+              <div className="plorem-ipsum-is">
+                PLorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard...
+              </div>
+            </div>
+            <button className="read-more-wrapper">
+              <div className="read-more">Read More</div>
+            </button>
+          </div>
+        </article>
+        <article className="frame-section">
+          <div className="frame-wrapper">
+            <div className="frame-div">
+              <div className="snacks-for-travel-parent">
+                <b className="grilled-tomatoes-at">Crunchwrap Supreme</b>
+                <div className="plorem-ipsum-is">
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <button className="read-more-wrapper">
+                <div className="read-more">Read More</div>
+              </button>
+            </div>
+          </div>
+          <img
+            className="meal-prep-ideas-846x846-1-icon"
+            alt=""
+            src="/mealprepideas846x846-12@2x.png"
+          />
+        </article>
+        <article className="frame-parent1">
+          <div className="frame-wrapper">
+            <div className="frame-div">
+              <div className="snacks-for-travel-parent">
+                <b className="grilled-tomatoes-at">Broccoli Cheese Soup</b>
+                <div className="plorem-ipsum-is">
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <button className="read-more-frame">
+                <div className="read-more">Read More</div>
+              </button>
+            </div>
+          </div>
+          <img
+            className="meal-prep-ideas-846x846-1-icon"
+            alt=""
+            src="/mealprepideas846x846-13@2x.png"
+          />
+        </article>
+      </article>
+    </div>
+  );
+};
+
+export default LandingPageDesktop;
